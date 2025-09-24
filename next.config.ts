60daf37ea3ad71b77d211edb228c571f76bed95e
Artem Dybov <dybov.artem.v@gmail.com>
2025-09-24 12:43:16 +0300

    3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
 import type { NextConfig } from "next";
+import type { RuleSetRule } from "webpack";
+
+function isRuleSetRule(rule: unknown): rule is RuleSetRule {
+	return typeof rule === "object" && rule !== null && "test" in rule;
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	webpack(config) {
+		const fileLoaderRule = config.module.rules.find(isRuleSetRule);
+
+		if (!fileLoaderRule) {
+			throw new Error("File loader rule not found");
+		}
+
+		config.module.rules.push(
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/,
+			},
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: {
+					not: [...((fileLoaderRule.resourceQuery as any)?.not ?? []), /url/],
+				},
+				use: ["@svgr/webpack"],
+			},
+		);
+
+		fileLoaderRule.exclude = /\.svg$/i;
+
+		return config;
+	},
+	images: {
+		domains: ["images.unsplash.com"],
+	},
 };
 
 export default nextConfig;
